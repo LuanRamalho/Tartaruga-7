93cb8d51dc9591907b3730c65be9a3a04f68d1e6
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-04-23 19:31:15 -0300

    Add files via upload

---
+++ Tartaruga 7.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from turtle import *  # importa a biblioteca
+color('blue', 'green')  # escolhe a cor do traco e do preenchimento
+begin_fill()  # inicia o desenho na posicao (0,0)
+forward(200)  # move 200 passos a frente
+left(90)  # vira a esquerda 90 graus
+forward(200)  # move 200 passos a frente
+left(90)  # vira a esquerda 90 graus
+forward(200)  # move 200 passos a frente
+left(90)  # vira a esquerda 90 graus
+forward(200)  # move 200 passos a frente
+end_fill()  # preenche o desenho
+done()  # encerra o process
